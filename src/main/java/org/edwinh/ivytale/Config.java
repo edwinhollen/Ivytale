e997801f1d1c4e91c7ccd5726e6aa74ca2b1f846
Edwin Hollen <edwinhollen@gmail.com>
2015-04-21 08:52:58 -0400

    Added config loading and parsing.

--- src/main/java/org/edwinh/ivytale/Config.java
+++ src/main/java/org/edwinh/ivytale/Config.java
@@ -1,5 +1,7 @@
 package org.edwinh.ivytale;
 
+import org.newdawn.slick.Input;
+
 import java.io.*;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
@@ -10,13 +12,15 @@ import java.nio.file.StandardCopyOption;
  */
 public class Config {
     public static final String CONFIG_FILE_LOCATION = System.getProperty("user.home")+"/.ivytale/config.txt";
+
+    public int screenWidth, screenHeight;
+    public int control_walkLeft, control_walkRight, control_attack;
+
     public Config(){
         load();
     }
-    public static void parse(String str){
 
-    }
-    public static void load(){
+    public void load(){
         File f = new File(CONFIG_FILE_LOCATION);
         if(!f.exists()){
             // see if ~/.ivytale/ exists
@@ -37,5 +41,46 @@ public class Config {
                 e.printStackTrace();
             }
         }
+        parse(f);
+    }
+
+    public void parse(File f){
+        try {
+            for(String l : Files.readAllLines(f.toPath())){
+                String line = l.trim().toUpperCase();
+                if(!(line.startsWith("#") || line.isEmpty())){
+                    String key = line.split("=")[0];
+                    String val = line.split("=")[1];
+                    switch(key){
+                        case "WIDTH":
+                            screenWidth = Integer.parseInt(val);
+                            break;
+                        case "HEIGHT":
+                            screenHeight = Integer.parseInt(val);
+                            break;
+                        case "WALK_LEFT":
+                            control_walkLeft = getKeyCode(val);
+                            break;
+                        case "WALK_RIGHT":
+                            control_walkRight = getKeyCode(val);
+                            break;
+                        case "ATTACK":
+                            control_attack = getKeyCode(val);
+                            break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getKeyCode(String keyName){
+        try {
+            return Integer.parseInt(Input.class.getField(keyName).get(Input.class).toString());
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        return -1;
     }
 }
